5bc589d60838dfa7167477d57279fd9861de79e2
Laxman Thayalan <laxman.thayalan@epita.fr>
2019-06-09 15:49:51 +0200

    Remove this.state/setState usages

--- src/components/tweetForm.js
+++ src/components/tweetForm.js
@@ -7,23 +7,16 @@ let id = 0
 class TweetForm extends React.Component {
     constructor() {
         super();
-        this.state = {
-          content: ""
-        };
         this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({ content : event.target.value });
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        const content = this.state.content;
+        const content = document.getElementById('input-tweet').value;
         if (content) {
             ++id;
             this.props.actionSubmit({ id, content });
-            this.setState({ content: "" });
+            document.getElementById('input-tweet').value = ""
         }
     }
 
@@ -31,10 +24,9 @@ class TweetForm extends React.Component {
         return  <form onSubmit={this.handleSubmit}>
                     <div className = "form-group">
                         <input
+                            id="input-tweet"
                             type = "text"
                             className = "form-control"
-                            value={this.state.content}
-                            onChange={(e) => this.handleChange(e)}
                         />
                         <button type="submit"
                                 className="btn btn-success btn-lg">Send tweet</button>
